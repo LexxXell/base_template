6430feb6acf41b3d3989a1cadbc2d9e360015247
LexxXell <lexxxell007@gmail.com>
2022-02-05 20:13:58 +0400

    Added Redis DB and its support on Python platform/

--- python/code/app.py
+++ python/code/app.py
@@ -1,3 +1,35 @@
 #!/usr/bin/env python
 
-print("Hello World!")
+from os import environ as env
+from redis import Redis
+
+redis_host = env.get("REDIS_HOST", "redis")
+
+def check_connection():
+    """ Connect to the Redis server """
+    try:
+        
+        # connect to the Redis server
+        print('Connecting to the Redis server...')
+        conn = Redis(host=redis_host)
+        
+	    # execute a statement
+        print('Redis server version:')
+        redis_info = conn.info()
+
+        # display the Redis server version
+        print(f"Redis version: {redis_info['redis_version']}; " + \
+            f"Redis mode: {redis_info['redis_mode']}; " + \
+                f"GCC version: {redis_info['gcc_version']}; " + \
+                    f" OS: {redis_info['os']} [x{redis_info['arch_bits']}]"
+        )
+
+        print("[INFO] Connection success!")
+
+    except Exception as error:
+        print(error)
+
+
+
+if __name__ == '__main__':
+    check_connection()
