2f2f689eef5a5dffdf5e22a8f2c45bf95d79ddb0
LexxXell <lexxxell007@gmail.com>
2022-02-09 19:53:16 +0400

    Added styles for html

--- html/js/main.js
+++ html/js/main.js
@@ -4,17 +4,19 @@ const baseApiURL = {
 }
 const checkPostgres = "check_postgres";
 const checkRedis = "check_redis";
+const info_header = document.querySelector("#info-header")
 
-function setApiCheckResults(baseURL, apiInfoObj, postgresInfoObj, redisInfoObj) {
+function setApiCheckResults(baseURL, apiInfoObj, postgresInfoObj, redisInfoObj, api_callback) {
     const apiData = httpGetJSON(baseURL);
     const postgresData = httpGetJSON(baseURL + checkPostgres);
     const redisData = httpGetJSON(baseURL + checkRedis);
     apiInfoObj.textContent = apiData.message ? apiData.message : apiInfoObj.textContent;
-    apiInfoObj.className = apiData.error ? "i_t__p_e" : "i_t__p_s";
+    apiInfoObj.className = apiData.error ? "--p-err" : "--p-scs";
+    if (api_callback) api_callback(apiData.error);
     postgresInfoObj.textContent = postgresData.message ? postgresData.message : postgresInfoObj.textContent;
-    postgresInfoObj.className = postgresData.error ? "i_t__p_e" : "i_t__p_s";
+    postgresInfoObj.className = postgresData.error ? "--p-err" : "--p-scs";
     redisInfoObj.textContent = redisData.message ? redisData.message : redisInfoObj.textContent;
-    redisInfoObj.className = redisData.error ? "i_t__p_e" : "i_t__p_s";
+    redisInfoObj.className = redisData.error ? "--p-err" : "--p-scs";
 }
 
 function httpGetJSON(theUrl) {
@@ -32,12 +34,26 @@ setApiCheckResults(
     baseURL = baseApiURL["python"],
     apiInfoObj = document.querySelector('#api_py_info'),
     postgresInfoObj = document.querySelector('#postgres_py_info'),
-    redisInfoObj = document.querySelector('#redis_py_info')
+    redisInfoObj = document.querySelector('#redis_py_info'),
+    api_callback = (api_error) => {
+        if (api_error) {
+            document.querySelector('#py-div').style.display = "none";
+        } else {
+            info_header.style.display = "none";
+        }
+    }
 )
 
 setApiCheckResults(
     baseURL = baseApiURL["node"],
     apiInfoObj = document.querySelector('#api_no_info'),
     postgresInfoObj = document.querySelector('#postgres_no_info'),
-    redisInfoObj = document.querySelector('#redis_no_info')
+    redisInfoObj = document.querySelector('#redis_no_info'),
+    api_callback = (api_error) => {
+        if (api_error) {
+            document.querySelector('#no-div').style.display = "none";
+        } else {
+            info_header.style.display = "none";
+        }
+    }
 )
