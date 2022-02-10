7b2288fb9a05aa2f56c97dc598a18318dd8e0798
LexxXell <lexxxell007@gmail.com>
2022-02-10 17:23:13 +0400

    Added NGINX as web server and reverse proxy

--- nodejs/code/app.js
+++ nodejs/code/app.js
@@ -1,1 +1,13 @@
-console.log("Hello WOrld!")
+const express = require("express");
+const app = express();
+const port = process.env.API_PORT ? process.env.API_PORT : 8000;
+
+app.get("/api_v1_no/", (req, res) => {
+    var _msg = "API.v1 is available";
+    var _error = false;
+    res.json({ error: _error, message: _msg });
+});
+
+app.listen(port, () => {
+    console.log(`The app listening on port ${port}`)
+});
