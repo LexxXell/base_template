1382fe547fc64ebed6c3f852b786f973903015d5
LexxXell <lexxxell007@gmail.com>
2022-02-11 19:00:39 +0400

    Added Redis server and its support on NodeJS platform

--- nodejs/code/app.js
+++ nodejs/code/app.js
@@ -1,1 +1,14 @@
-console.log("Hello WOrld!")
+const Redis = require("ioredis");
+
+const redis = new Redis(host = process.env.REDIS_HOST ? process.env.REDIS_HOST : "localhost");
+
+async function getRedisInfo() {
+    const redisInfo = [];
+    const _redisInfo = (await redis.info("Server")).split("\r\n");
+    [1, 5, 6, 7].forEach(i => redisInfo.push(_redisInfo[i]));
+    return `Redis: ${redisInfo.join(", ")}`;
+}
+
+(async() => {
+    console.log(await getRedisInfo());
+})();
