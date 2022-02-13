38e8ec2f70b9b8b1d6f92626a4816fe0a75c15fe
LexxXell <lexxxell007@gmail.com>
2022-02-14 02:09:00 +0400

    Added NGINX as web server and revers proxy.

--- python/code/app.py
+++ python/code/app.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python
 
-print("Hello World!")
+from os import environ as env
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+api_port = env.get("API_PORT", 8000)
+
+@app.route("/api_v1_py/")
+def check_api():
+    return jsonify({ "error": False, "message": "API.v1 is available" })
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=api_port)
