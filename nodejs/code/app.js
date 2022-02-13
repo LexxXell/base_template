d117794d7f9ec1812c9f5f11fe3aa730d90617f3
LexxXell <lexxxell007@gmail.com>
2022-02-13 19:50:22 +0400

    Added PostgreSQL and its support on Python and NodeJS platforms

--- nodejs/code/app.js
+++ nodejs/code/app.js
@@ -1,1 +1,15 @@
-console.log("Hello WOrld!")
+const { Client } = require("pg");
+
+const client = new Client({
+    host: process.env.POSTGRES_HOST ? process.env.POSTGRES_HOST : "localhost",
+    user: process.env.POSTGRES_USER ? process.env.POSTGRES_USER : "postgres",
+    password: process.env.POSTGRES_PASSWORD ? process.env.POSTGRES_PASSWORD : "postgres",
+    database: process.env.POSTGRES_DB ? process.env.POSTGRES_DB : "postgres",
+    port: 5432
+})
+
+client.connect()
+client.query('SELECT version()', (err, res) => {
+    console.log(err ? "PostgreSQL " + err.message : res.rows[0]["version"]);
+    client.end();
+});
--- python/code/app.py
+++ python/code/app.py
@@ -1,3 +1,45 @@
 #!/usr/bin/env python
+import psycopg2
+from os import environ as env
 
-print("Hello World!")
+pg_host = env.get("POSTGRES_HOST", "postgres")
+pg_user = env.get("POSTGRES_USER", "postgres")
+pg_pass = env.get("POSTGRES_PASSWORD","postgres")
+pg_db = env.get("POSTGRES_DB","postgres")
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(
+            host=pg_host,
+            database=pg_db,
+            user=pg_user,
+            password=pg_pass
+        )
+		
+        # create a cursor
+        cur = conn.cursor()
+        
+	    # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+       
+	    # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+if __name__ == '__main__':
+    connect()
